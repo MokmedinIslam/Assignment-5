cb9c330891ba541e08bd9a35e6b471b900938c9c
Mokmedin <mokmedini@gmail.com>
2024-09-24 22:47:50 +0600

    add home page

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
 document.getElementById('btn-login')
     .addEventListener('click', function(){
-        window.location.href = '/home.html'
+        window.location.href = 'https://mokmedinislam.github.io/Assignment-5/Home.html'
     });
